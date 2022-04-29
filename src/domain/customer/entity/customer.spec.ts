5c1127d2a313d4b2829b6be30d29887b44ec8531
LeonardoMortari <leo_mortari2008@hotmail.com>
2022-04-28 21:08:33 -0300

    Adição do pattern notification no projeto

--- src/domain/customer/entity/customer.spec.ts
+++ src/domain/customer/entity/customer.spec.ts
@@ -7,7 +7,7 @@ describe("Customer unit tests", () => {
 
         expect(() => {
             let customer = new Customer("", "John");
-        }).toThrowError("Id is required");
+        }).toThrowError("customer: Id is required");
 
     });
 
@@ -15,7 +15,15 @@ describe("Customer unit tests", () => {
 
         expect(() => {
             let customer = new Customer("123", "");
-        }).toThrowError("Name is required");
+        }).toThrowError("customer: Name is required");
+
+    });
+
+    it("should throw error when name and id is empty", () => {
+
+        expect(() => {
+            let customer = new Customer("", "");
+        }).toThrowError("customer: Id is required,customer: Name is required");
 
     });
 
